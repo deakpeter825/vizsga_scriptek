b4f53261582a86af5d6111e863c56cf110b6a077
deakpeter825 <43757300+deakpeter825@users.noreply.github.com>
2019-04-15 17:22:57 +0200

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -17,33 +17,8 @@ sudo chmod 755 /srv/readonly
 
 cd /etc/samba
 sudo mv /etc/samba/smb.conf /etc/samba/smb_old.conf
+cp ./smb.conf /etc/samba/smb.conf
 
-touch smb.conf
-
-echo " [global]
-netbios name=samba
-security=user
-map to guest=bad user
-workgroup = WORKGROUP
-public=yes" > etc/samba/smb.conf
-
-
-echo " [kozos]
-comment=nyilvános írható-olvasható megosztás
-path=/srv/kozos
-writeable=yes
-read only=no
-browseable=yes
-guest ok=yes
-public=yes
-
-[readonly]
-comment=nyilvános csak olvasható megosztás
-path=/srv/readonly
-read only=yes
-browseable=yes
-guest ok=yes
-public=yes" >> etc/samba/smb.conf
 
 sudo service smbd restart
 sudo service smbd status
@@ -54,17 +29,6 @@ sudo mkdir /srv/user2
 sudo chown user2 /srv/user2
 sudo chmod 700 /srv/user2
 
-
-echo "[user2]
-comment=írható-olvasható megosztás a user2 felhasználónak
-path=/srv/user2
-writeable=yes
-browseable=no
-public=no
-read list=user2
-writelist=user2
-force directory mode=0777
-force create mode=0777" >> etc/samba/smb.conf
 
 sudo smbpasswd -a user2 hallgato #adjuk meg a jelszót: hallgato
 
